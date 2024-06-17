7dffa635c0b5df3ba665093c44219b8d41020579
javiertmezac <javiermeza.cazarez@gmail.com>
2024-06-17 10:45:32 -0700

    WIP - create login for persona
    modal to capture login. validators for pass and confirmpass

--- src/app/persona/edit/persona-list.component.ts
+++ src/app/persona/edit/persona-list.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter,  Output,  inject } from '@angular/core';
-import { FormGroup,  ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Persona } from 'src/app/model/persona';
 import { PersonaService } from 'src/app/shared/persona.service';
+import { MatchPassword } from './match-password.validator';
 
 @Component({
   selector: 'cc-persona-list',
@@ -13,6 +14,7 @@ import { PersonaService } from 'src/app/shared/persona.service';
   styleUrl: './persona-list.component.scss'
 })
 export class PersonaListComponent {
+  passwordPattern = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$';
   people: Persona[] = []
   personaService = inject(PersonaService)
   modalService = inject(NgbModal)
@@ -20,7 +22,20 @@ export class PersonaListComponent {
   selectedPersona: Persona = this.personaService.emptyPersona();
   personaLoginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
   ngOnInit() {
+
+    const formOptions: AbstractControlOptions = {
+      validators: MatchPassword('pass', 'passconfirm')
+    };
+
+    this.personaLoginForm = this.fb.group({
+      name: ['', Validators.required],
+      pass: ['', [Validators.required, Validators.pattern(this.passwordPattern)]],
+      passconfirm: ['', Validators.required]
+    }, formOptions);
+
     this.fetchPeople();
   }
 
@@ -35,21 +50,25 @@ export class PersonaListComponent {
     this.selectedPersonaEvent.emit(persona);
   }
 
-
-
   assignLoginAccount(content: any, persona: Persona) {
     this.selectedPersona = persona;
     this.openModal(content);
   }
 
   openModal(content: any) {
-		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
-	}
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (value) => this.clearForm(),
+      (reason) => this.clearForm()
+    );
+  }
+
+  clearForm() {
+    this.personaLoginForm.reset();
+  }
 
   onSubmit() {
 
   }
 
-  cancel() {}
-  generateRandomPassword(persona: Persona){}
+  generateRandomPassword(persona: Persona) { }
 }
